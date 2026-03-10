e8a231e4909d9f4f3ebc0663fc0f5fd2058aa3f2
okerekeo-a11y <okerekeo@students.trinitydc.edu>
2026-03-09 22:54:32 -0400

    Create set_operations.py

---
+++ set_operations.py
@@ -0,0 +1,12 @@
+"""
+Ogechukwu Okereke
+CSMC 111
+Spring 2026
+week6 assignment3
+"""
+numbers = {1, 2, 2, 3, 4}
+print("Original set:", numbers)
+numbers.add(5)
+numbers.remove(3)
+print("Updated set:", numbers)
+print("Is 2 in the set?", 2 in numbers)
