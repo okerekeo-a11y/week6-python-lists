081877b3ebdad3eb176cb04c7ccd25cdae7395a2
okerekeo-a11y <okerekeo@students.trinitydc.edu>
2026-03-09 22:41:09 -0400

    Create list_operations.py

---
+++ list_operations.py
@@ -0,0 +1,13 @@
+"""
+Ogechukwu Okereke
+CSMC 111
+Spring 2026
+week6 assignment1
+"""
+numbers = [1,2,3,4,5,6,7,8,9,10]
+print("Original list:", numbers)
+numbers.append(11)
+numbers.remove(5)
+print("Updated list:", numbers)
+first_five = numbers[:5]
+print("First five elements:", first_five)
