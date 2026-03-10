a869d3675da435c7a18559fe4e0b96e1f9e395b6
okerekeo-a11y <okerekeo@students.trinitydc.edu>
2026-03-09 22:50:43 -0400

    Create tuple_practice.py

---
+++ tuple_practice.py
@@ -0,0 +1,10 @@
+"""
+Ogechukwu Okereke
+CSMC 111
+Spring 2026
+week6 assignment2
+"""
+cities = ("New York", "Los Angeles", "Chicago")
+print("Cities:", cities)
+print("Second city:", cities[1])
+cities[1] = "Houston"
